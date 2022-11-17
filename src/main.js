bd18b71d58b5b9f520784f38568807c0ea99f0b9
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-11-17 18:24:01 -0300

    Requisito 04

--- src/main.js
+++ src/main.js
@@ -7,10 +7,24 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const produtos = document.querySelector('.products'); // section dos produtos
 
+const addCarregando = () => {
+  const criaElemento = document.createElement('h1');
+  criaElemento.innerHTML = 'Carregando...';
+  criaElemento.classList.add('loading');
+  produtos.appendChild(criaElemento);
+};
+addCarregando();
+
 const listaDeProdutos = await fetchProductsList('computador'); // traz o array do objeto do computador
 
-// createProductElement(produtos); --> cria os produtos na grade
+const retiraCarregando = () => {
+  const carregando = document.querySelector('.loading');
+  carregando.remove();
+};
+retiraCarregando();
 
 listaDeProdutos.forEach((item) => {
   produtos.appendChild(createProductElement(item));
 });
+
+// createProductElement(produtos); --> cria os produtos na grade
