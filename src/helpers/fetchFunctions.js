c18ee9f0a0cf51d722549527f1bb69d61c760e8c
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-11-18 16:13:31 -0300

    Requisito 07

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,6 +1,17 @@
-export const fetchProduct = () => {
-  // seu código aqui
+// const { resultado } = data;
+
+export const fetchProduct = async (busca1) => {
+  if (!busca1) {
+    throw new Error('ID não informado');
+  }
+  const url = `https://api.mercadolibre.com/items/${busca1}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const resultado = data;
+  return resultado;
 };
+// const text = await fetchProduct('MLB1405519561');
+// console.log(text);
 
 export const fetchProductsList = async (busca) => {
   if (!busca) {
